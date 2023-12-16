d440d0ef460bd3b3cf7304804ff387972e085b96
Manas Hejmadi <manas2all@outlook.com>
2023-12-16 15:17:19 +0530

    Credentials & API_Extract added

--- lib/backend/healthscore.dart
+++ lib/backend/healthscore.dart
@@ -6,4 +6,14 @@ import 'package:patientpulse/main.dart';
 
 final bearerTokenProvider = StateProvider<String?>((ref) => null);
 
+class HealthScoreCredentials {
+  static const String _target = 'https://hackathon.hsdevonline.com';
+  static const String hsLoginURL = '$_target/HSLogin';
+  static const String baseURL = '$_target/HS/mobile/V2';
+  static const String hospitalCode = 'CSOFTSUPPORT';
+  static const String clientSecret =
+      r'$2a$10$iNssdGZSSfsmS9lzireKNelKNKDNQ/octZGZ.qh9nGy1u7St2ChC2';
+  static const String clientID = 'hackathon';
+}
+
 class HealthScoreAPI {}
